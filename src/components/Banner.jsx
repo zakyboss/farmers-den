4077daf9c80c4abded9e01981e1da79360d455d7
Zakyboss <Boochimo8@gmail.com>
2025-02-25 17:26:09 +0300

    Banner completed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,9 @@
-import styles from './Banner.module.css'
+import styles from "./Banner.module.css";
 export default function Banner() {
-  return <div className={styles.container}>Banner</div>;
+  return (
+    <div className={styles.container}>
+      <span>📍 Nairobi South B </span>
+      <span>Boochimo9@gmail.com</span>
+    </div>
+  );
 }
